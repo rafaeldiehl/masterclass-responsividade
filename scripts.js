661088cc584ade08757be81337b890fc3402c3d0
Rafael <rafaeljuliani1984@gmail.com>
2020-12-12 00:55:16 -0300

    feat: Add menu hamburger function

--- scripts.js
+++ scripts.js
@@ -77,3 +77,14 @@ document.querySelector(".close-modal").addEventListener("click", () => {
   modal.querySelector("iframe").src = ``;
   document.querySelector("body").style.overflow = "initial";
 });
+
+// Responsive menu
+let show = true;
+const menuSection = document.querySelector('.menu-section');
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+  menuSection.classList.toggle('on', show);
+  show =! show;
+});
